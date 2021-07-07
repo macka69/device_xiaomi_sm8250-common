fcf0faca8bb25f3931a4368854914bed8d2938a5
johnmart19 <johnivan19999@gmail.com>
2021-07-08 01:42:03 +0300

    sm8250: Build offline charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>

--- kona.mk
+++ kona.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
