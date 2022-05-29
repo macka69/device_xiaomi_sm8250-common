dbb03b99489cd0458643d205fdb068e4b0b04689
Mesquita <mesquita@aospa.co>
2022-05-29 13:09:47 +1000

    sm8250-common: Switch to perf qti component
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- kona.mk
+++ kona.mk
@@ -351,10 +351,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
@@ -381,6 +377,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor
+
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
 
 # Rootdir
 PRODUCT_PACKAGES += \
